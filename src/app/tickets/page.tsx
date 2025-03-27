3fba7c7ed14e836f7e8a7b08ea0d93c8fca09fed
Duong Thanh Tan <nobudy55@gmail.com>
2025-03-28 00:04:40 +0700

    Finished section typescript lector

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -2,6 +2,12 @@ import Link from 'next/link'
 import { initialTickets } from '@/data'
 import { ticketPath } from '@/path'
 
+const TICKET_ICONS = {
+  OPEN: 'O',
+  DONE: 'X',
+  IN_PROGRESS: '>'
+}
+
 export default function TicketsPage() {
   return (
     <div>
@@ -10,6 +16,7 @@ export default function TicketsPage() {
         {initialTickets.map(ticket => (
           <li key={ticket.id}>
             <h2>{ticket.title}</h2>
+            <p>{TICKET_ICONS[ticket.status]}</p>
             <Link href={ticketPath(ticket.id)} className="underline">View</Link>
           </li>
         ))}
--- src/data.ts
+++ src/data.ts
@@ -3,24 +3,25 @@ export const initialTickets = [
     id: "1",
     title: "Create a new project",
     content: "Create a new project using Angular CLI",
-    status: "OPEN",
+    status: "OPEN" as const
   },
   {
     id: "2",
     title: "Create a new component",
     content: "Create a new component using Angular CLI",
-    status: "OPEN",
+    status: "OPEN" as const
   },
   {
     id: "3",
     title: "Create a new service",
     content: "Create a new service using Angular CLI",
-    status: "OPEN",
+    status: "IN_PROGRESS" as const
   },
   {
     id: "4",
     title: "Create a new module",
     content: "Create a new module using Angular CLI",
-    status: "DONE",
+    status: "DONE" as const
   },
+
 ]
