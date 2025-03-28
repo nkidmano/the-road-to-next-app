5a384a683350a28533858f3372a343b423d3ddbd
Duong Thanh Tan <nobudy55@gmail.com>
2025-03-28 10:49:49 +0700

    Finished section styling lector

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,11 @@ import { ticketsPath } from '@/path'
 
 export default function HomePage() {
   return (
-    <div>
-      <p className="text-lg">Home page</p>
-      <Link className="underline" href={ticketsPath()}>Go to tickets</Link>
+    <div className="flex flex-col gap-y-8">
+      <h2 className="text-3xl font-semibold tracking-tight">Home page</h2>
+      <div className="text-center">
+        <Link className="underline" href={ticketsPath()}>Go to tickets</Link>
+      </div>
     </div>
   );
 }
