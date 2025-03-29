b72e53798cd0e06e6169cc5817907f6cb9c0d59e
Duong Thanh Tan <nobudy55@gmail.com>
2025-03-30 03:25:12 +0700

    Finished section Redirect

--- src/features/ticket/actions/delete-ticket.ts
+++ src/features/ticket/actions/delete-ticket.ts
@@ -1,7 +1,11 @@
 "use server";
 
+import { redirect } from "next/navigation";
 import { prisma } from "@/lib/prisma";
+import { ticketsPath } from "@/path";
 
 export async function deleteTicket(id: string) {
   await prisma.ticket.delete({ where: { id } });
+
+  redirect(ticketsPath());
 }
